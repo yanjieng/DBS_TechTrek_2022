45efcc760efd90be2180fa2093f77e6fa14802d0
darrenlim <darrenlimweijie@gmail.com>
2022-01-20 14:26:53 +0800

    front end home

---
+++ social-app1/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+﻿import "./sidebar.css";
+import {
+    Home,
+    Person,
+} from "@material-ui/icons";
+
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <Home className="sidebarIcon" />
+                        <span className="sidebarListItemText">Home</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Person className="sidebarIcon" />
+                        <span className="sidebarListItemText">Profile</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
---
+++ social-app1/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+﻿import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import "./home.css"
+
+export default function Home() {
+    return (
+        <>
+            
+            <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+            
+            </div>
+        </>
+    );
+}
